e64caef9cdcee86a727e25b6e8ff01fc18b6c485
thejtzone <67770231+thejtzone@users.noreply.github.com>
2026-01-18 18:50:19 +1030

    debugging

--- static/js-tools/TimetableMaker.js
+++ static/js-tools/TimetableMaker.js
@@ -17,7 +17,10 @@ function createTimetable(data) {
     const div = document.querySelector(`div#${DIV_ID}`);
     if (!div) return false;
 
+    console.log(data)
     data = isStringified(data || "[]") ? JSON.parse(data || "[]") : (data || []);
+    console.log(data);
+    
     const earliest = data.legnth > 0 ? data.sort((a, b) => a.start - b.start)[0].start : "none";
     const latest = data.legnth > 0 ? data.sort((a, b) => b.end - a.end)[0].end : "none";
 
