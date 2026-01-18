de9a00a7fb39adcc23ee6062f6803e85a3943455
thejtzone <67770231+thejtzone@users.noreply.github.com>
2026-01-18 17:34:10 +1030

    help?

--- static/js-tools/TimetableMaker.js
+++ static/js-tools/TimetableMaker.js
@@ -21,12 +21,12 @@ function createTimetable() {
 
     div.style.display = "grid";
     div.style.gridTemplateColumns = `min-content repeat(7, 1fr)`;
-    div.style.gridTemplateRows = `repeat(calc(24 * 60), min-content)`;
+    div.style.gridTemplateRows = `repeat(${24 * 60}, min-content)`;
 
     const blanks = []
     for (let i = 0; i < 7; i++) {
         let blank = document.createElement("div");
-        blank.style.gridArea = `${i + 2} / 1 / span ${24 * 60} / span 1`;
+        blank.style.gridArea = `1 / ${i + 2} / ${24 * 60} / ${i + 2}`;
         blanks.push(blank);
         div.appendChild(blank);
     }
