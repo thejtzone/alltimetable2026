b7eaec0965ea1436383bc124a451e4e3c27f243d
thejtzone <67770231+thejtzone@users.noreply.github.com>
2026-01-20 11:36:49 +1030

    text size

--- static/js-tools/TimetableMaker.js
+++ static/js-tools/TimetableMaker.js
@@ -190,7 +190,7 @@ function createTimetable(data) {
         if (realHeight > realWidth) eventDiv.style.writingMode = "vertical-lr";
         eventDiv.textContent = event.name;
         eventDiv.style.textIndent = "2ch";
-        eventDiv.style.fontSize = "1.7rem";
+        eventDiv.style.fontSize = "1.4rem";
 
         // Ensure text fits
         if (eventDiv.offsetWidth > realWidth) {
